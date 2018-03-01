ce9dbb7309ea6b5cdbce468dc8a473634dbdc115
Kyle Mayers <kyle7150@gmail.com>
2018-03-01 22:04:10 +0000

    finish course 2

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,10 +1,13 @@
+var dbconn = require('../data/dbconnection.js');
+var ObjectId = require('mongod').ObjectId;
 var hotelData = require('../data/hotel-data.json');
 
 module.exports.hotelsGetAll = function(req, res) {
-     console.log("GET the hotels");
-     console.log(req.query);
-     
-     var offset =0;
+
+    var db = dbconn.get();
+    var collection = db.collection('hotels');
+    
+    var offset =0;
      var count = 5;
      
      if (req.query && req.query.offset) {
@@ -12,27 +15,40 @@ module.exports.hotelsGetAll = function(req, res) {
      }
      
      if (req.query && req.query.offset) {
-        count = parseInt(req.query.count, 10);
+        count = parseInt(req.query.offset, 10);
      }
-     
-     var returnData = hotelData.slice(offset,offset+count);
-     
-        res
-        .status(200)
-        .json( returnData );
+    
+    collection
+        .find()
+        .skip(offset)
+        .limit(count)
+        .toArray(function(err, docs) { 
+            console.log("found hotels", docs);
+            res
+            .status(200)
+            .json(docs);
+        });
 };
 
 module.exports.hotelsGetOne = function(req, res) {
+    var db = dbconn.get();
+    var collection = db.collection('hotels');
+    
     var hotelId = req.params.hotelId;
-    var thisHotel = hotelData[hotelId];
     console.log("GET hotelId", hotelId);
-    res
-        .status(200)
-        .json(thisHotel);
+    
+    collection
+        .findOne({
+            _id : ObjectId(hotelId)
+        }, function(err, doc) {
+        res
+            .status(200)
+            .json( doc);  
+        });
 };
 
 module.exports.hotelsAddOne = function(req, res) {
-    console.log("POST new hotel");
+    console.log("PST new hotel");
     console.log(req.body);
     res
         .status(200)
