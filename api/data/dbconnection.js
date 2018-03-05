b9956c7825e012505f46111cd3b3439c0e50f413
Kyle Mayers <kyle7150@gmail.com>
2018-03-05 22:38:45 +0000

    section 3 udemy

--- api/data/dbconnection.js
+++ api/data/dbconnection.js
@@ -4,21 +4,21 @@ var dburl = 'mongodb://localhost:27017/meanhotel';
 var _connection = null;
 
 var open = function() {
-    MongoClient.connect(dburl, function(err, client) {
-        if (err) {
-            console.log("DB connection failed");
-            return;
-        }
-        _connection = client.db("meanhotel");
-    console.log("DB connection open", client);
-    });
+  MongoClient.connect(dburl, function(err, db) {
+    if (err) {
+      console.log("DB connection failed");
+      return;
+    }
+    _connection = db;
+    console.log("DB connection open");
+  });
 };
 
 var get = function() {
-    return _connection;
+  return _connection;
 };
 
 module.exports = {
-    open : open,
-    get : get
+  open : open,
+  get : get
 };
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,17 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var ctrlHotels = require('../controllers/hotels.controllers.js');
+var ctrlReviews = require('../controllers/reviews.controllers.js');
 
 router
     .route('/hotels')
-    .get(ctrlHotels.hotelsGetAll);
+    .get(ctrlHotels.hotelsGetAll)
+    .post(ctrlHotels.hotelsAddOne);
 
 router
     .route('/hotels/:hotelId')
     .get(ctrlHotels.hotelsGetOne);
     
+    //Review Routes
 router
-    .route('/hotels/new')
-    .post(ctrlHotels.hotelsAddOne);
+    .route('/hotels/:hotelId/reviews')
+    .get(ctrlReviews.reviewsGetAll);
+    
+router
+    .route('/hotels/:hotelId/reviews/:reviewId')
+    .get(ctrlReviews.reviewsGetOne);
 
 module.exports = router;
