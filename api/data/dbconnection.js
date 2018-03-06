3645f530bb45f2bb50c1dfdd1794e99c90de18ab
Kyle Mayers <kyle7150@gmail.com>
2018-03-06 07:43:24 +0000

    section 3 complete

--- api/data/dbconnection.js
+++ api/data/dbconnection.js
@@ -1,17 +1,17 @@
 var MongoClient = require('mongodb').MongoClient;
-var dburl = 'mongodb://localhost:27017/meanhotel';
+var dburl = 'mongodb://' + process.env.ip + ' :27017';
 
 var _connection = null;
 
 var open = function() {
-  MongoClient.connect(dburl, function(err, db) {
-    if (err) {
-      console.log("DB connection failed");
-      return;
-    }
-    _connection = db;
-    console.log("DB connection open");
-  });
+    MongoClient.connect(dburl, function(err, client) {
+        if (err) {
+            console.log("DB connection failed");
+            return;
+        }
+        _connection = client.db("meanhotel");
+    console.log("DB connection open", client);
+    });
 };
 
 var get = function() {
--- app.js
+++ app.js
@@ -8,18 +8,26 @@ var routes = require('./api/routes');
 
 app.set('port', process.env.PORT);
 
+// Add middleware to console log every request
 app.use(function(req, res, next) {
-    console.log(req.method, req.url);
-    next();
+  console.log(req.method, req.url);
+  next();
 });
 
+// Set static directory before defining routes
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+app.use('/fonts', express.static(__dirname + '/fonts'));
 
-app.use(bodyParser.urlencoded({ extended : false }));
+// Enable parsing of posted forms
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+// Add some routing
 app.use('/api', routes);
 
+// Listen for requests
 var server = app.listen(app.get('port'), function() {
-    var port = server.address().port;
-console.log("magic happens on port " + port);
+  var port = server.address().port;
+  console.log('Magic happens on port ' + port);
 });
